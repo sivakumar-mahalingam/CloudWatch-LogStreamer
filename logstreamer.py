9bd6ffc865d72adaabcea8219b37eb598b0614c6
Sivakumar Mahalingam <sivasai2006@gmail.com>
2024-01-22 22:08:04 +0400

    alignment fix

--- logstreamer.py
+++ logstreamer.py
@@ -48,6 +48,7 @@ response = logs.describe_log_streams(
 	limit = 1
 )
 
+
 # Post log to an existing Log Stream using uploadSequenceToken
 if len(response['logStreams']) > 0 and response['logStreams'][0] and 'uploadSequenceToken' in response['logStreams'][0]:
     sequence_token = response['logStreams'][0]['uploadSequenceToken']
@@ -72,7 +73,7 @@ else:
 		)
 		
 	# Post log to a new Log Stream
-	response_log_events = logs.put_log_events(
+    response_log_events = logs.put_log_events(
 	  logGroupName = LOG_GROUP,
 	  logStreamName = LOG_STREAM,
 	  logEvents = [
